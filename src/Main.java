0ef578566bacad2fbd66169e0bccee913245bce6
LeoKliemann <lgkliemann05@gmail.com>
2026-02-18 17:25:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//Shipping Cost Calculator Pseudocode:
+//class ShippingCosts
+//	main()
+//		num itemPrice
+//		num shippingCost
+//		num totalPrice
+//		output “What is the price of your item?”
+//		input itemPrice
+//		if itemPrice < 100 then
+//			shippingCost = itemPrice * .02
+//			totalPrice = shippingCost + itemPrice
+//			output “Your shipping cost is” + shippingCost “and your total cost is” + totalPrice
+//		else
+//			output “Your total cost is” + itemPrice
+//		endIf
+//	return
+//endClass
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0;
+        double shippingCost;
+        double totalPrice;
+        String trash = "";
+    }
+}
